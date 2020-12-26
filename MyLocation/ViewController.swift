447503289de26b00e0cc3adf80dff77f5d105d6a
Anna Taylor <annatnyc@gmail.com>
2020-12-26 17:10:05 -0500

    Initial Commit

---
+++ MyLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocation
+//
+//  Created by Anna Taylor on 12/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
